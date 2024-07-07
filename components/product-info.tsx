c12d3c7199a9e2ca50aed01d1d9088b125b38894
blakej115 <blakej115@gmail.com>
2024-07-06 21:15:13 -0400

    Added data fetching for product img and title

--- components/product-info.tsx
+++ components/product-info.tsx
@@ -1,8 +1,17 @@
-export default function ProductInfo() {
+import getData from "@/lib/get-data";
+import Image from "next/image";
+
+export default async function ProductInfo() {
+  const {
+    data: { product },
+  } = await getData("product");
+
   return (
     <>
-      <div className="aspect-square w-full bg-black"></div>
-      <h2 className="text-[24px] font-bold">Apple</h2>
+      <div className="aspect-square w-full">
+        <Image src={product.img} width={300} height={300} alt={product.title} />
+      </div>
+      <h2 className="text-[24px] font-bold">{product.title}</h2>
     </>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: process.env.IMGS_PROTOCOL,
+        hostname: process.env.IMGS_HOSTNAME,
+        port: process.env.IMGS_PORT,
+        pathname: process.env.IMGS_PATHNAME,
+      },
+    ],
+  },
+};
 
 export default nextConfig;
