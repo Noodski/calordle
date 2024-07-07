bbe04acf2a947754ecb1e9a0e52ce9629c8c6093
blakej115 <blakej115@gmail.com>
2024-07-06 21:41:06 -0400

    Added favicon, Colored text in header

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,9 @@
 export default function Header() {
   return (
     <header className="flex justify-center bg-gray px-8 py-4">
-      <h1 className="text-center text-[30px] font-bold">Calordle</h1>
+      <h1 className="rounded-md bg-blue px-2 text-center text-[30px] font-bold text-white">
+        Calordle
+      </h1>
     </header>
   );
 }
