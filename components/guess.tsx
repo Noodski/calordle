4996e8b5aff97601c920e75846347b7415c1196b
blakej115 <blakej115@gmail.com>
2024-08-10 14:53:42 -0400

    Removed unused code from guess component

--- components/guess.tsx
+++ components/guess.tsx
@@ -1,50 +1,15 @@
 "use client";
 
-import { useGuesses, useGuessesDispatch } from "@/context/guesses";
-import { useProgress, useProgressDispatch } from "@/context/progress";
-import { useState, ChangeEvent, MouseEvent } from "react";
+import { useProgress } from "@/context/progress";
+import { useState, ChangeEvent } from "react";
 import Button from "./button";
 
 export default function Guess() {
   const [value, setValue] = useState<string>("");
-  const guesses = useGuesses();
-  const dispatchGuess = useGuessesDispatch();
   const progress = useProgress();
-  const dispatchProgress = useProgressDispatch();
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
-  };
-
-  const handleSubmit = (event: MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-
-    setValue("");
-
-    let guess = Number(value);
-
-    /* dispatchGuess?.({
-      type: "added",
-      value: guess,
-      status:
-        guess > correctGuess
-          ? "lower"
-          : guess < correctGuess
-            ? "higher"
-            : "correct",
-    });
-
-    if (guess === correctGuess) {
-      dispatchProgress?.({
-        type: "changed",
-        value: "success",
-      });
-    } else if (guesses?.length === 5) {
-      dispatchProgress?.({
-        type: "changed",
-        value: "fail",
-      });
-    } */
   };
 
   if (progress === "success" || progress === "fail") return null;
@@ -58,7 +23,7 @@ export default function Guess() {
         value={value}
         onChange={handleInputChange}
       />
-      <Button onClick={handleSubmit}>Submit</Button>
+      <Button type="submit">Submit</Button>
     </form>
   );
 }
